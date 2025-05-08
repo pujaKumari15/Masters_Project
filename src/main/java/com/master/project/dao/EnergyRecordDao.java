5df98fa18768628ae5c35ba1e29c5977caaf4f21
pujakumari <puja.kumari@sjsu.edu>
2025-05-08 11:41:42 -0700

    Added api for engery record

---
+++ src/main/java/com/master/project/dao/EnergyRecordDao.java
@@ -0,0 +1,16 @@
+package com.master.project.dao;
+
+import com.master.project.model.EnergyRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+public interface EnergyRecordDao extends JpaRepository<EnergyRecord, String> {
+
+    // Custom query to get energy records filtered by user_id and date range
+    @Query("SELECT e FROM EnergyRecord e WHERE e.userId = :userId AND e.timestamp BETWEEN :startDate AND :endDate")
+    List<EnergyRecord> findByUserIdAndDateRange(String userId, Timestamp startDate, Timestamp endDate);
+}
+
---
+++ src/main/java/com/master/project/service/EnergyRecordService.java
@@ -0,0 +1,21 @@
+package com.master.project.service;
+
+import com.master.project.dao.EnergyRecordDao;
+import com.master.project.model.EnergyRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.sql.Timestamp;
+import java.util.List;
+
+@Service
+public class EnergyRecordService {
+
+    @Autowired
+    private EnergyRecordDao energyRecordRepository;
+
+    // Method to get energy records filtered by user_id and date range
+    public List<EnergyRecord> getEnergyRecordsByUserIdAndDateRange(String userId, Timestamp startDate, Timestamp endDate) {
+        return energyRecordRepository.findByUserIdAndDateRange(userId, startDate, endDate);
+    }
+}
+
